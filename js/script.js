45166fac3f05a394297819b395e7135e57efeaf8
zzq229 <zzqwaterloo@gmail.com>
2017-09-27 04:41:15 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$('nav a').click(function () {
+	  var $href = $(this).attr('href');
+	  $('body').stop().animate({
+	    scrollTop: $($href).offset().top
+	  }, 500);
+	  return false;
+	});
+});
+
+function initMap() {
+	var uluru = {lat: 43.4736285, lng: -80.53703009999998};
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+         map: map
+        });
+}
+   
+  
