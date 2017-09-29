d4cf5046ce1b60555f694fb3b8a5e9cdb41874bb
zzq229 <zzqwaterloo@gmail.com>
2017-09-28 22:51:01 -0400

    modify index

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 function initMap() {
 	var uluru = {lat: 43.4736285, lng: -80.53703009999998};
     var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
+    zoom: 18,
     center: uluru
     });
     var marker = new google.maps.Marker({
